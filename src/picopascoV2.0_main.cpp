192855981cb3de60ebbfc84ed5880b23d8af66f8
Jason Russell <grayhatguy@grayhatguy.com>
2023-02-25 02:49:52 -0500

    Update picopascoV2.0_main.cpp

--- src/picopascoV2.0_main.cpp
+++ src/picopascoV2.0_main.cpp
@@ -1,7 +1,3 @@
-/*
-1) add sensors recompile
-2) try sgp test or old scan*/
-
 /* picopasco 
  *  Sensor monitor and control firmware for an economical and scalable multinutrient growing system using an RP2040 pico and standard off the shelf sensors and components. Reads air temp, humidity, VOC, CO2, Soil H2O, TDS, EC, and water temp sensors interval per t_loop. Dispenses nutrient solutions based upon user set points: fill volume and concentrations of contituents. 
  *  additional libraries required for a successfull build: https://github.com/Seeed-Studio/SGP30_Gas_Sensor * https://www.arduinolibraries.info/libraries/sensirion-i2-c-sht4x
